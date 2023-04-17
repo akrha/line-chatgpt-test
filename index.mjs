9b28402081f6564194c03f1404b66fdbebd0379c
Akira Hasegawa <hase.a1995@gmail.com>
2023-04-17 10:54:55 +0800

    Add ChatGPT function

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import line from '@line/bot-sdk';
+import { Configuration, OpenAIApi } from "openai";
 
 // LINE Messaging APIのチャンネルアクセストークンを環境変数から取得
 const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
@@ -8,13 +9,28 @@ const client = new line.Client({
     channelAccessToken: channelAccessToken
 });
 
+// OpenAIのAPIキーを環境変数から取得
+const apiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+    apiKey: apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
 export const handler = async (event) => {
     const eventObject = event.events[0];
     if (eventObject.type === 'message' && eventObject.message.type === 'text') {
-        // TODO:
+        // ChatGPTに質問して応答を取得
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "user", content: eventObject.message.text } // ユーザーの入力をChatGPTに送信
+            ],
+        });
+
+        // ChatGPTの応答をLINEに返信
         await client.replyMessage(eventObject.replyToken, {
             type: 'text',
-            text: eventObject.message.text
+            text: completion.data.choices[0].message.content
         });
     }
 };
